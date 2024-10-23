85bada74c19c6c0b917defffcdfc4a299394044c
Canh Nguyen <canh.nh@eoh.io>
2024-10-23 17:52:09 +0700

    first commit

---
+++ CloundController.js
@@ -0,0 +1,75 @@
+import Clound from "./Clound.js";
+
+export default class CloundController {
+  CACTUS_INTERVAL_MIN = 4000;
+  CACTUS_INTERVAL_MAX = 6000;
+
+  cacti = [];
+
+  constructor(ctx, cactiImages, scaleRatio, speed) {
+    this.ctx = ctx;
+    this.canvas = ctx.canvas;
+    this.cactiImages = cactiImages;
+    this.scaleRatio = scaleRatio;
+    this.speed = speed;
+
+    this.setNextCactusTime();
+  }
+
+  setNextCactusTime() {
+    const num = this.getRandomNumber(
+      this.CACTUS_INTERVAL_MIN,
+      this.CACTUS_INTERVAL_MAX
+    );
+
+    this.nextCactusInterval = num;
+  }
+
+  getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  createCactus() {
+    const offset = this.getRandomNumber(this.canvas.height - 100, this.canvas.height - 50);
+    const cactusImage = this.cactiImages[0];
+    const x = this.canvas.width * 1.5;
+    let y =  this.canvas.height - cactusImage.height - offset;
+  
+    const cactus = new Clound(
+      this.ctx,
+      x,
+      y,
+      cactusImage.width,
+      cactusImage.height,
+      cactusImage.image
+    );
+
+    this.cacti.push(cactus);
+  }
+
+  update(gameSpeed, frameTimeDelta) {
+    if (this.nextCactusInterval <= 0) {
+      this.createCactus();
+      this.setNextCactusTime();
+    }
+    this.nextCactusInterval -= frameTimeDelta;
+
+    this.cacti.forEach((cactus) => {
+      cactus.update(this.speed, gameSpeed, frameTimeDelta, this.scaleRatio);
+    });
+
+    this.cacti = this.cacti.filter((cactus) => cactus.x > -cactus.width);
+  }
+
+  draw() {
+    this.cacti.forEach((cactus) => cactus.draw());
+  }
+
+  collideWith(sprite) {
+    return this.cacti.some((cactus) => cactus.collideWith(sprite));
+  }
+
+  reset() {
+    this.cacti = [];
+  }
+}
